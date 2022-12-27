97662a1217aed7041d7e208f3e2f01fed68d84a5
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2022-12-27 16:42:38 +0100

    (feat/app/search-bar) :init FEAT

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import { Component } from "react";
 import "./App.css";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       humans: [],
-      searchField: "",
+      searchFied: "",
     };
   }
-
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { humans: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
+      .then((users) => {
+        this.setState(() => {
+          return { humans: users };
+        });
+      });
   }
   render() {
-    const filterdHumans = this.state.humans.filter((human) => {
-      return human.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const filteredList = this.state.humans.filter((human) => {
+      return human.name.toLowerCase().includes(this.state.searchFied);
     });
     return (
       <div className="app">
         <input
-          className="search-box"
-          type="search"
-          placeholder="search humans"
-          onChange={(event) => {
+          className="search-bar"
+          placeholder="search..."
+          onChange={(event) =>
             this.setState(() => {
-              return { searchField: event.target.value };
-            });
-          }}
+              return { searchFied: event.target.value.toLowerCase() };
+            })
+          }
         />
-        {filterdHumans.map(function (human) {
+        {filteredList.map((human) => {
           return <h1 key={human.id}>{human.name}</h1>;
         })}
       </div>
