a2d0da5428ff928e939800609705715f66f403f0
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2022-12-27 16:14:01 +0100

    (feat/app/search-bar) :init FEAT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      humans: [],
+      searchField: "",
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { humans: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  render() {
+    const filterdHumans = this.state.humans.filter((human) => {
+      return human.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className="app">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search humans"
+          onChange={(event) => {
+            this.setState(() => {
+              return { searchField: event.target.value };
+            });
+          }}
+        />
+        {filterdHumans.map(function (human) {
+          return <h1 key={human.id}>{human.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
