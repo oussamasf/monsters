5df2ebc22ddf4f651fd57bcc524ef19a39145fe9
ousf <ousf@mail.com>
2022-12-29 18:31:45 +0100

    style

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,16 +1,16 @@
-import { Component} from "react"
-
+import { Component } from "react";
+import "./search-bar.style.css";
 class SearchBar extends Component {
- render() {
-   return (
-    <input
-    className={this.props.className}
-    type="search"
-    placeholder={this.props.placeholder}
-    onChange={this.props.onChangeHandler}
-  />
-   )
- }
+  render() {
+    return (
+      <input
+        className={`search-box ${this.props.className}`}
+        type="search"
+        placeholder={this.props.placeholder}
+        onChange={this.props.onChangeHandler}
+      />
+    );
+  }
 }
 
-export default SearchBar
+export default SearchBar;
